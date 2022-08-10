6ee431f8d10e44b54ff301a414a2af86fba85415
NiboshiDaifuku <53576995+NiboshiDaifuku@users.noreply.github.com>
2022-08-11 05:00:56 +0900

    使い方を修正

--- src/App.js
+++ src/App.js
@@ -17,9 +17,13 @@ export default function App() {
         <li>動画タブを開く</li>
         <li>最古の動画までスクロールする</li>
         <li>F12を押して開発ツールを開く</li>
-        <li>HTML要素を丸コピする</li>
-        <li>テキストボックスに張り付ける（10秒ほどかかる）</li>
-        <li>解析開始ボタンを押す</li>
+        <li>動画のitem一覧を格納している親要素を探し、要素をコピーする</li>
+        <ul>
+          <li>サイトの作りが変わらない限りは、以下の要素をコピーすればOK</li>
+          <li>{`<div id="items" class="style-scope ytd-grid-renderer">`}</li>
+        </ul>
+        <li>テキストボックスに張り付ける</li>
+        <li>解析開始ボタンを押す（※2回押さないと動かないときがある）</li>
         <li>csvファイルが出力されるので自由に使う</li>
       </ol>
       <hr />
